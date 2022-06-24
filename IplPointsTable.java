9b4bb9c213142f4f52a323d66ab7f8b33b708699
Anjan Kumar B H <60666602+anjankumarbh@users.noreply.github.com>
2022-06-24 07:58:44 +0530

    3rd commit with complete modification in the DS

--- IplPointsTable.java
+++ IplPointsTable.java
@@ -2,102 +2,90 @@ package javaInterviewQuestions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class IplPointsTable {
-	static Map<String,Object> m;
-	Team t;
+	
+	static List<Team> teamList;//using List as a data structure to store the team data
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		m=new HashMap<String,Object>();
-		Scanner sc=new Scanner(System.in);
-		Scanner str=new Scanner(System.in);
-		System.out.println("How many teams you want : ");
-		int noOfTeams=sc.nextInt();
-		String teamName="";
-		int points=0;
-		String [] status= {};
-		for(int i=0;i<noOfTeams;i++) {
-			status=new String[5];
-			System.out.println("Enter the team name");
-			teamName=str.nextLine();
-			System.out.println("Enter the points");
-			points=sc.nextInt();
-			System.out.println("Enter the status of five matches [W as win] or [L as Lose]");
-			for(int j=0;j<5;j++) {
-				status[j]=str.nextLine().toUpperCase();
-			}
-			m.put(teamName, new Team(points,status));	
-			status=null;
-			
-		}
-		System.out.println("  Teams\tPoints\tLast 5 Matches\n");
-		for(String key:m.keySet()) {
-			System.out.println(" "+key+" "+m.get(key));
-		}
-		System.out.println("Enter the consicutive status you want to search please enter W or L");
-		String WorL =str.nextLine().toUpperCase();
-		System.out.println("Enter how many consicutives");
-		int noOfConsecutive=sc.nextInt();
-		List consicutiveTeams=new ArrayList();;
-		int avgOfConsucitiveTeams=0;
-		int count=0;
-		for (Map.Entry<String,Object> entry : m.entrySet()) {
-	          // System.out.println("Key = " + entry.getKey());
-	           //System.out.println("Value = " + entry.getValue());
-	           Team t = (Team) entry.getValue();
-	         //  System.out.println("Points :" + t.getPoints());
-	           if( t.consecutive(t.getStatusOfFivs(),noOfConsecutive,WorL)==true) {
-	        	   consicutiveTeams.add(entry.getKey());
-	        	   avgOfConsucitiveTeams+=t.getPoints();
-	        	   count++;
-	           }
-	           
-        }
-		avgOfConsucitiveTeams/=count;
-		System.out.println("Teams having consicutive "+noOfConsecutive+" "+WorL+" are "+Arrays.asList(consicutiveTeams));
-		System.out.println("Average of these filterd teams is "+avgOfConsucitiveTeams);
 		
+		teamList=new ArrayList<Team>();
+		//adding all 10 teams data to the list 
+		teamList.add(new Team("RCB",23, new String[] {"L","W","L","W","W"}));
+		teamList.add(new Team("GT",22, new String[] {"W","L","W","L","L"}));
+		teamList.add(new Team("LSG",24, new String[] {"W","W","L","W","L"}));
+		teamList.add(new Team("RR",18, new String[] {"L","L","W","L","W"}));
+		teamList.add(new Team("DC",16, new String[] {"L","W","W","L","W"}));
+		teamList.add(new Team("KKR",20, new String[] {"W","L","L","W","L"}));
+		teamList.add(new Team("PBKS",23, new String[] {"W","L","W","W","L"}));
+		teamList.add(new Team("SRH",12, new String[] {"L","W","L","L","W"}));
+		teamList.add(new Team("CSK",14, new String[] {"L","L","L","L","L"}));
+		teamList.add(new Team("MI",22, new String[] {"L","L","L","L","L"}));
+		//sorting the Team objects based on their points and printing them
+		teamList.sort(Comparator.comparing(Team::getPoints));
+		System.out.println(" Teams\tPoints\tLast5Status \n");
+		for(int i=teamList.size()-1;i>=0;i--) {
+			System.out.println(teamList.get(i));
+		}
+		//taking the input to perform 2 consecutive win or loss
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the team with 2 consecutive wins or loss u want please enter W or L :\n");
+		String consecutiveUserInput=input.nextLine().toUpperCase();
+		
+		int totalPointsOfConsecutiveTeams=0;//variable to add the points of teams comes under requested consecutive category
+		
+		List<String> teamsWithMatchingConsecutives=new ArrayList<String>();// a list to store requested consecutive teams 
+		
+		for(int j=0;j<teamList.size();j++) {//Looping over the List of team
+			for(int k=0;k<4;k++) {//looping over the statusOfFive in Team class
+				if(teamList.get(j).getStatusOfFivs(k).equals(consecutiveUserInput) && teamList.get(j).getStatusOfFivs(k+1).equals(consecutiveUserInput)) {
+					totalPointsOfConsecutiveTeams+=teamList.get(j).getPoints();
+					teamsWithMatchingConsecutives.add(teamList.get(j).getTeamName());
+					break;
+				}
+				
+			}
+		}
+		
+		System.out.println("Teams with 2 consicutive "+consecutiveUserInput+" are : ");
+		for(String teams:teamsWithMatchingConsecutives) {//printing the obtained teams under consecutive category
+			System.out.println(teams);
+		}
+		//printing the average of points with those teams with the requested status of win or loss
+		System.out.println("Average points of these teams is : "+(totalPointsOfConsecutiveTeams/teamsWithMatchingConsecutives.size()));	
 			
 	}
 
 }
-class Team{
+class Team{//Team class to store all the team data
+	
+	String teamName;
 	int points;
 	String[] statusOfFive;
-	Team(int point,String[] s){
-		this.points=point;
-		this.statusOfFive=s;
+	
+	Team(String teamName,int points,String[] statusOfFive){
+		this.teamName=teamName;
+		this.points=points;
+		this.statusOfFive=statusOfFive;
 	}
 	public String toString() {
-		return ("  "+this.points+"\t"+
-			Arrays.asList(this.statusOfFive));
+		return (" "+this.teamName+"\t"+this.points+"\t"+
+			Arrays.toString(this.statusOfFive));
+	}
+	public String getTeamName() {
+		return this.teamName;
 	}
 	public int getPoints() {
 		return this.points;
 	}
-	public List getStatusOfFivs() {
-		return  (Arrays.asList(statusOfFive));
-	}
-	public boolean consecutive(List<String> s,int n,String m) {
-		int count=0;
-		int temp=0;
-		for(int i=n;i<s.size();i++) {
-			temp=n;
-			while(temp>=0) {
-				if(s.get(count).equals(m)) {
-					count++;
-				}
-				if(count==n) {
-					return true;
-				}
-				temp--;
-			}
-		}
-		return false;
-	
+	public String getStatusOfFivs(int n) {
+		return  statusOfFive[n];
 	}
 }
+
